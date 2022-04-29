777196e4feb56471b897383e7a6dda8d6fd05bdf
tallywoop <efthimisgeorgiadis17@gmail.com>
2022-04-29 20:09:46 +0300

    making the children (reproduction)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from cost_calculator import CostCalculator
 import random
-
+from cost_calculator import CostCalculator
+from reproduction import Reproduction
 
 def city_shuffle():
     path = "A"
@@ -49,3 +49,13 @@ for j in range(len(trial_path)):
     random_samples[j] = random.uniform(0, totalChance)
     random_samples[j] = parent_picker(random_samples[j], totalChance, og_population_idx, trial_path)
 print(random_samples)
+
+#make new generation
+child = random_samples
+child[0] = "".join(Reproduction.generate_child(random_samples[0], random_samples[1]))
+child[1] = "".join(Reproduction.generate_child(random_samples[1], random_samples[0]))
+child[2] = "".join(Reproduction.generate_child(random_samples[2], random_samples[3]))
+child[3] = "".join(Reproduction.generate_child(random_samples[3], random_samples[2]))
+print(child)
+
+
