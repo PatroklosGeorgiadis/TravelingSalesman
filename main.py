87d9a269898665eedda4b2af1032b892e63d4e88
tallywoop <efthimisgeorgiadis17@gmail.com>
2022-04-30 00:00:57 +0300

    small changes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import random
 from cost_calculator import CostCalculator
 from reproduction import Reproduction
+from itertools import groupby
+
 
 def city_shuffle():
     path = "A"
@@ -14,14 +16,6 @@ def city_shuffle():
     return path
 
 
-'''
-totalS (totalChance)
-random_sample
-og_population_idx
-path
-'''
-
-
 def parent_picker(self, totalS, idx, path):
     while idx >= 0:
         totalS -= CostCalculator.path_cost(path[idx])
@@ -31,28 +25,39 @@ def parent_picker(self, totalS, idx, path):
             idx -= 1
 
 
-#creating the original population (N = 4)
-paths = [city_shuffle(), city_shuffle(), city_shuffle(), city_shuffle()]
+def all_equal(paths_list):
+    res = groupby(paths_list)
+    return next(res, True) and not next(res, False)
+
+
+#creating the original population (amount N)
+paths = []
+for N in range(4):
+    paths.append(city_shuffle())
+
+#main algorithm
 generations = 0
-while not(paths[0] == paths[1] == paths[2] == paths[3]):
+pathsN = len(paths)
+#convergence process
+while not(all_equal(paths)):
     totalChance = 0
     print("Generation " + str(generations) + ": " + str(paths))
 
     #calculating the cost of each path
-    for og_population_idx in range(len(paths)):
+    for og_population_idx in range(pathsN):
         print(CostCalculator.path_cost(paths[og_population_idx]))
         totalChance += CostCalculator.path_cost(paths[og_population_idx])
-    print(totalChance)
+    #print(totalChance)
 
     #picking randomly the new parents
-    random_samples = [0, 0, 0, 0]
-    for j in range(len(paths)):
+    random_samples = [0]*pathsN
+    for j in range(pathsN):
         random_samples[j] = random.uniform(0, totalChance)
         random_samples[j] = parent_picker(random_samples[j], totalChance, og_population_idx, paths)
     print(random_samples)
 
     #making the new generation of children
-    for child_idx in range(0, len(paths), 2):
+    for child_idx in range(0, pathsN, 2):
         paths[child_idx] = "".join(Reproduction.generate_child(random_samples[child_idx], random_samples[child_idx+1]))
         paths[child_idx+1] = "".join(Reproduction.generate_child(random_samples[child_idx+1], random_samples[child_idx]))
     generations += 1
