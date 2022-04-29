71cd7a45142ec2eae73a34600d79911a0c5b9563
tallywoop <efthimisgeorgiadis17@gmail.com>
2022-04-29 20:47:13 +0300

    convergence of the children

--- main.py
+++ main.py
@@ -32,30 +32,28 @@ def parent_picker(self, totalS, idx, path):
 
 
 #creating the original population (N = 4)
-trial_path = [city_shuffle(), city_shuffle(), city_shuffle(), city_shuffle()]
-totalChance = 0
-og_population_idx = 0
-print(trial_path)
+paths = [city_shuffle(), city_shuffle(), city_shuffle(), city_shuffle()]
+generations = 0
+while not(paths[0] == paths[1] == paths[2] == paths[3]):
+    totalChance = 0
+    print("Generation " + str(generations) + ": " + str(paths))
 
-#calculating the cost of each path
-for og_population_idx in range(len(trial_path)):
-    print(CostCalculator.path_cost(trial_path[og_population_idx]))
-    totalChance += CostCalculator.path_cost(trial_path[og_population_idx])
-print(totalChance)
+    #calculating the cost of each path
+    for og_population_idx in range(len(paths)):
+        print(CostCalculator.path_cost(paths[og_population_idx]))
+        totalChance += CostCalculator.path_cost(paths[og_population_idx])
+    print(totalChance)
 
-#picking randomly the new parents
-random_samples = [0, 0, 0, 0]
-for j in range(len(trial_path)):
-    random_samples[j] = random.uniform(0, totalChance)
-    random_samples[j] = parent_picker(random_samples[j], totalChance, og_population_idx, trial_path)
-print(random_samples)
+    #picking randomly the new parents
+    random_samples = [0, 0, 0, 0]
+    for j in range(len(paths)):
+        random_samples[j] = random.uniform(0, totalChance)
+        random_samples[j] = parent_picker(random_samples[j], totalChance, og_population_idx, paths)
+    print(random_samples)
 
-#make new generation
-child = random_samples
-child[0] = "".join(Reproduction.generate_child(random_samples[0], random_samples[1]))
-child[1] = "".join(Reproduction.generate_child(random_samples[1], random_samples[0]))
-child[2] = "".join(Reproduction.generate_child(random_samples[2], random_samples[3]))
-child[3] = "".join(Reproduction.generate_child(random_samples[3], random_samples[2]))
-print(child)
-
-
+    #make new generation
+    paths[0] = "".join(Reproduction.generate_child(random_samples[0], random_samples[1]))
+    paths[1] = "".join(Reproduction.generate_child(random_samples[1], random_samples[0]))
+    paths[2] = "".join(Reproduction.generate_child(random_samples[2], random_samples[3]))
+    paths[3] = "".join(Reproduction.generate_child(random_samples[3], random_samples[2]))
+    generations += 1
