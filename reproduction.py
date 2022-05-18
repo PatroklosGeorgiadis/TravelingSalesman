03c24242dede7adb391717010728c3b6784c6dfc
tallywoop <efthimisgeorgiadis17@gmail.com>
2022-05-18 19:16:14 +0300

    Return to regular single-point

--- reproduction.py
+++ reproduction.py
@@ -1,6 +1,3 @@
-import random
-
-
 class Reproduction:
 
     def generate_child(self, partner):
@@ -10,29 +7,22 @@ class Reproduction:
             self: The first parent
             partner: The second parent
         """
-        # single point crossover (random amount of cities from each parent)
-        crossover_point = random.randint(1, 3) + 1
+        # single point crossover (2 cities from 1st parent, 2 cities from second)
+
         # all the cities that will be used from the 1st parent
-        visited = [self[5]]
-        for a in range(crossover_point):
-            visited.insert(a, self[a])
+        visited = [self[0], self[1], self[2], self[5]]
         # all the cities that will be used, if not already, from the 2nd parent
-        partner_cities = [partner[1], partner[2], partner[3], partner[4]]
-        for b in range(crossover_point - 1, 4):
-            updt_idx = 0
-            temp = partner_cities[b]
-            partner_cities.pop(b)
-            partner_cities.insert(updt_idx, temp)
-            updt_idx += 1
-        print("visited: " + str(visited) + " partner_cities: " + str(partner_cities))
+        partner_cities = [partner[3], partner[4], partner[1], partner[2]]
         # repeat until all cities of the 2nd parent are implemented in the child trip
         for x in range(len(partner_cities)):
             # if the city is already in the child trip, skip
             if partner_cities[x] in visited:
                 continue
             else:
-                # insert the city in the child trip
-                visited.insert(crossover_point, partner_cities[x])
-                # increase the index of the next city to be inserted by 1
-                crossover_point += 1
+                # the first time a city from the 2nd parent isn't included, it is inserted as the 4th city in the child
+                if len(visited) == 4:
+                    visited.insert(3, partner_cities[x])
+                # the second time a city from the 2nd parent isn't included, it is inserted as the 5th
+                else:
+                    visited.insert(4, partner_cities[x])
         return visited
